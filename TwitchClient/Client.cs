3f814ad6376876f1cccda80992ac1650b629b5e8
Trevor Morris <tremorris1999@gmail.com>
2022-10-12 03:19:45 +0000

    chore: adds client and clip downloads

---
+++ TwitchClient/Client.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PogPillSharp.TwitchClient;
+
+public class Client
+{
+  private readonly HttpClient _httpClient;
+  private readonly string _clientId;
+  private readonly string _clientSecret;
+  private Token _token;
+  public Token Token { get => _token; }
+
+  public Client(string clientId, string clientSecret)
+  {
+    _clientId = clientId;
+    _clientSecret = clientSecret;
+    _token = new(_clientId, _clientSecret);
+    _httpClient = new();
+    _httpClient.DefaultRequestHeaders.Add("Client-Id", _clientId);
+    _httpClient.DefaultRequestHeaders.Accept.Add(new("application/json"));
+  }
+
+  public void GetClip(string url)
+  {
+    string downloadUri = GetClipDownloadUri(url);
+  }
+
+  private string GetClipDownloadUri(string url)
+  {
+    Regex idGrabber = new(@"(?<=https:\/\/clips.twitch.tv\/).*");
+    string id = idGrabber.Match(url).Value;
+    HttpRequestMessage request = new()
+    {
+      Method = HttpMethod.Get,
+      RequestUri = new($"https://api.twitch.tv/helix/clips?id={id}")
+    };
+
+    request.Headers.Authorization = new("Bearer", _token.AccessToken);
+    HttpResponseMessage response = Task.Run(async () => await _httpClient.SendAsync(request)).Result;
+    if(response.StatusCode is System.Net.HttpStatusCode.Unauthorized)
+    {
+      RefreshToken();
+      request = new()
+      {
+        Method = HttpMethod.Get,
+        RequestUri = new($"https://api.twitch.tv/helix/clips?id={id}")
+      };
+
+      request.Headers.Authorization = new("Bearer", _token.AccessToken);
+      response = Task.Run(async () => await _httpClient.SendAsync(request)).Result;
+    }
+
+    if(!response.IsSuccessStatusCode)
+      throw new ApplicationException("Unable to fetch clip.");
+
+    JsonNode? responseData = JsonSerializer.Deserialize<JsonNode>(response.Content.ReadAsStream());
+    string? downloadUri = responseData?["data"]?[0]?["thumbnail_url"]?
+      .GetValue<string>()?
+      .Replace("-preview-480x272.jpg", ".mp4");
+
+      return downloadUri ?? throw new ApplicationException("Unable to deserialize clip object.");
+  }
+
+  private void RefreshToken()
+  {
+    _token = new(_clientId, _clientSecret);
+  }
+}
---
+++ TwitchClient/Token.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace PogPillSharp.TwitchClient;
+
+public class Token
+{
+  private const string TWITCH_BASE_URI = "https://id.twitch.tv/oauth2/token";
+  public string AccessToken { get; }
+
+  public Token(string clientId, string clientSecret)
+  {
+    HttpClient client = new();
+    HttpRequestMessage request = new()
+    {
+      Method = HttpMethod.Post,
+      RequestUri = new($"{TWITCH_BASE_URI}?client_id={clientId}&client_secret={clientSecret}&grant_type=client_credentials")
+    };
+
+    HttpResponseMessage response = Task.Run(async () => await client.SendAsync(request)).Result;
+    if (!response.IsSuccessStatusCode)
+      throw new ApplicationException($"Unable to fetch token: {response.ReasonPhrase}");
+
+    JsonNode tokenData = JsonSerializer.Deserialize<JsonNode>(response.Content.ReadAsStream()) ?? throw new ApplicationException("Unable to deserialize response data."); 
+    AccessToken = tokenData["access_token"]?.GetValue<string>() ?? throw new ApplicationException("Unable to fetch token.");
+  }
+}
